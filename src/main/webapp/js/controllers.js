ea1f26f0e2528fd0806f1d00e16925b12610b2cf
EzeLunaWatkins <eilw2000@hotmail.com>
2016-05-27 19:17:28 -0300

    Actualización de Habitacion Actual en el html

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -8,14 +8,24 @@ app.controller('JuegoCtrl', function($scope, $http) {
 		$scope.jugador = response.data;
 	});
 
-	$scope.laberintoSeleccionado = null;
 	$scope.seleccionNula = true;
 	$scope.hayUnLaberintoSeleccionado = false;
 	$scope.seEstaJugandoEnElLaberintoSeleccionado = false;
+
+	$scope.laberintoSeleccionado = null;
+	$scope.habitacionActual = null;
 
 	$scope.seleccionarLaberinto = function(laberinto) {
 		$scope.laberintoSeleccionado = laberinto;
 		$scope.seleccionNula = false;
 		$scope.hayUnLaberintoSeleccionado = true;
+	};
+
+	$scope.jugarLaberinto = function() {
+		$scope.hayUnLaberintoSeleccionado = false;
+		$scope.seEstaJugandoEnElLaberintoSeleccionado = true;
+		$scope.laberintoSeleccionado.disponibilidad = false;
+		$scope.laberintoSeleccionado.jugadorActual = $scope.jugador;
+		$scope.laberintoSeleccionado.jugadorActual.habitacionActual = $scope.laberintoSeleccionado.habitacionInicial;
 	};
 });
